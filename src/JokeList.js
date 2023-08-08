e0e2632ebaa3b5f25eae1e5285c666ea23184446
Dean Lunn <dean.lunn82@gmail.com>
2023-08-08 13:15:17 -0600

    second commit

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,51 +1,142 @@
 import React, { Component } from "react";
-import "./Joke.css";
+import axios from "axios";
+import Joke from "./Joke";
+import "./JokeList.css";
 
-class Joke extends Component {
+class JokeList extends Component {
+  static defaultProps = {
+    numJokesToGet: 10,
+  };
+
   constructor(props) {
     super(props);
-    this.upVote = this.upVote.bind(this);
-    this.downVote = this.downVote.bind(this);
+    this.state = {
+      jokes: [],
+    };
+
+    this.generateNewJokes = this.generateNewJokes.bind(this);
+    this.resetVotes = this.resetVotes.bind(this);
     this.toggleLock = this.toggleLock.bind(this);
+    this.vote = this.vote.bind(this);
   }
 
-  upVote() {
-    this.props.vote(this.props.id, +1);
+  /* at mount, get jokes */
+
+  componentDidMount() {
+    if (this.state.jokes.length < this.props.numJokesToGet) this.getJokes();
   }
 
-  downVote() {
-    this.props.vote(this.props.id, -1);
+  componentDidUpdate() {
+    if (this.state.jokes.length < this.props.numJokesToGet) this.getJokes();
   }
 
-  toggleLock() {
-    this.props.toggleLock(this.props.id);
+  /* retrieve jokes from API */
+
+  async getJokes() {
+    try {
+      // load jokes one at a time, adding not-yet-seen jokes
+      let jokes = this.state.jokes;
+      let jokeVotes = JSON.parse(
+        window.localStorage.getItem("jokeVotes") || "{}"
+      );
+      let seenJokes = new Set(jokes.map((j) => j.id));
+
+      while (jokes.length < this.props.numJokesToGet) {
+        let res = await axios.get("https://icanhazdadjoke.com", {
+          headers: { Accept: "application/json" },
+        });
+        let { status, ...joke } = res.data;
+
+        if (!seenJokes.has(joke.id)) {
+          seenJokes.add(joke.id);
+          jokeVotes[joke.id] = jokeVotes[joke.id] || 0;
+          jokes.push({ ...joke, votes: jokeVotes[joke.id], locked: false });
+        } else {
+          console.log("duplicate found!");
+        }
+      }
+
+      this.setState({ jokes });
+      window.localStorage.setItem("jokeVotes", JSON.stringify(jokeVotes));
+    } catch (e) {
+      console.log(e);
+    }
   }
+
+  /* empty joke list, set to loading state, and then call getJokes */
+
+  generateNewJokes() {
+    this.setState((st) => ({ jokes: st.jokes.filter((j) => j.locked) }));
+  }
+
+  resetVotes() {
+    window.localStorage.setItem("jokeVotes", "{}");
+    this.setState((st) => ({
+      jokes: st.jokes.map((joke) => ({ ...joke, votes: 0 })),
+    }));
+  }
+
+  /* change vote for this id by delta (+1 or -1) */
+
+  vote(id, delta) {
+    let jokeVotes = JSON.parse(window.localStorage.getItem("jokeVotes"));
+    jokeVotes[id] = (jokeVotes[id] || 0) + delta;
+    window.localStorage.setItem("jokeVotes", JSON.stringify(jokeVotes));
+    this.setState((st) => ({
+      jokes: st.jokes.map((j) =>
+        j.id === id ? { ...j, votes: j.votes + delta } : j
+      ),
+    }));
+  }
+
+  toggleLock(id) {
+    this.setState((st) => ({
+      jokes: st.jokes.map((j) =>
+        j.id === id ? { ...j, locked: !j.locked } : j
+      ),
+    }));
+  }
+
+  /* render: either loading spinner or list of sorted jokes. */
 
   render() {
+    let sortedJokes = [...this.state.jokes].sort((a, b) => b.votes - a.votes);
+    let allLocked =
+      sortedJokes.filter((j) => j.locked).length === this.props.numJokesToGet;
+
     return (
-      <div className={`Joke ${this.props.locked ? "Joke-locked" : ""}`}>
-        <div className="Joke-votearea">
-          <button onClick={this.upVote}>
-            <i className="fas fa-thumbs-up" />
-          </button>
+      <div className="JokeList">
+        <button
+          className="JokeList-getmore"
+          onClick={this.generateNewJokes}
+          disabled={allLocked}
+        >
+          Get New Jokes
+        </button>
+        <button className="JokeList-getmore" onClick={this.resetVotes}>
+          Reset Vote Counts
+        </button>
 
-          <button onClick={this.downVote}>
-            <i className="fas fa-thumbs-down" />
-          </button>
+        {sortedJokes.map((j) => (
+          <Joke
+            text={j.joke}
+            key={j.id}
+            id={j.id}
+            votes={j.votes}
+            vote={this.vote}
+            locked={j.locked}
+            toggleLock={this.toggleLock}
+          />
+        ))}
 
-          <button onClick={this.toggleLock}>
-            <i
-              className={`fas ${this.props.locked ? "fa-unlock" : "fa-lock"}`}
-            />
-          </button>
-
-          {this.props.votes}
-        </div>
-
-        <div className="Joke-text">{this.props.text}</div>
+        {sortedJokes.length < this.props.numJokesToGet ? (
+          <div className="loading">
+            <i className="fas fa-4x fa-spinner fa-spin" />
+          </div>
+        ) : null}
       </div>
     );
   }
 }
 
-export default Joke;
+export default JokeList;
